e5ddeb8e6b0577d41c247d5a3666cacf0c734013
Gh0stage <61917966+Gh0stage@users.noreply.github.com>
2023-12-19 14:49:55 +0100

    add Entity inteface inheriting from IGameObject

--- Classes/Object/Entity/Player.cs
+++ Classes/Object/Entity/Player.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HandsOnDeck.Interfaces;
 
 namespace HandsOnDeck.Classes.Object.Entity
 {
-    internal class Player
+    internal class Player : IEntity
     {
     }
 }
