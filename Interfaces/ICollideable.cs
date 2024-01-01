78cf08a9bf86f10b24a57e880bde36ab35a0b8ce
afissou <afissou8@gmail.com>
2024-01-01 17:35:05 +0100

    add ICollideable

---
+++ Interfaces/ICollideable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HandsOnDeck.Classes.Collisions;
+
+namespace HandsOnDeck.Interfaces
+{
+    internal interface ICollideable
+    {
+        CollisionHandler CollisionHandler { get; }
+        bool CollidesWith(ICollideable other);
+    }
+}
