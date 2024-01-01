20c7cd9bee030a11bbc2d705f38510d2f335c09d
Gh0stage <61917966+Gh0stage@users.noreply.github.com>
2024-01-01 22:34:56 +0100

    Add moving sea tiles to the game

---
+++ Classes/Object/Static/Background.cs
@@ -0,0 +1,58 @@
+﻿using HandsOnDeck.Classes.Animations;
+using HandsOnDeck.Classes.Managers;
+using HandsOnDeck.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HandsOnDeck.Classes.Object.Static
+{
+    internal class Background : IGameObject
+    {
+        private static Background background;
+        private static object syncRoot = new object();
+        private static Animation water = new Animation("Swater", new Vector2(128, 128), 1, 6, 39, 1 / 39f, true);
+
+
+        public void LoadContent()
+        {
+            water.LoadContent();
+        }
+        public static Background GetInstance
+        {
+            get
+            {
+                if (background == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (background == null)
+                            background = new Background();
+                    }
+                }
+                return background;
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            water.Update(gameTime);
+        }
+
+        public void Draw()
+        {
+            GraphicsDevice _graphics = GraphicsDeviceSingleton.Instance;
+            SpriteBatch _spriteBatch = SpriteBatchManager.Instance;
+            Vector2 position = Vector2.Zero; // Adjust the position as needed
+            Vector2 screenSize = new Vector2(_graphics.Viewport.Width, _graphics.Viewport.Height);
+            water.Draw(position, screenSize);
+        }
+    }
+}
