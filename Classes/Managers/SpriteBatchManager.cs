8209eb832f2beb644d827151fc1e65f556831ce4
Gh0stage <61917966+Gh0stage@users.noreply.github.com>
2024-01-01 18:54:24 +0100

    add SpriteBatch Singleton for easier accesibility

---
+++ Classes/Managers/SpriteBatchManager.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnDeck.Classes.Managers
+{
+    public class SpriteBatchManager
+    {
+        private static SpriteBatch _spriteBatch;
+
+        public static void Initialize(SpriteBatch spriteBatch)
+        {
+            _spriteBatch = spriteBatch;
+        }
+
+        public static SpriteBatch Instance
+        {
+            get
+            {
+                return _spriteBatch;
+            }
+        }
+    }
+}
