f5d4ae15312a7cf7c4f5770e3085316d35ecc142
Gh0stage <61917966+Gh0stage@users.noreply.github.com>
2024-01-05 12:21:56 +0100

    Add Button and Toggle classes

---
+++ Classes/UI/UIElements/Button.cs
@@ -0,0 +1,57 @@
+﻿using HandsOnDeck;
+using HandsOnDeck.Classes.Managers;
+using HandsOnDeck.Classes.UI.UIElements;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+public class Button
+{
+    private string text;
+    private Vector2 position;
+    private Action action;
+    private SpriteFont font;
+    private Texture2D texture;
+    private Texture2D hoverTexture;
+    private Rectangle bounds;
+    private bool isHovered;
+    private bool isClicked;
+
+    public Button(string text, Vector2 position, Action action)
+    {
+        this.text = text;
+        this.position = position;
+        this.action = action;
+        this.font = Game1.DefaultFont;
+        this.texture = ContentLoader.Load<Texture2D>("button");
+        this.hoverTexture = ContentLoader.Load<Texture2D>("buttonH");
+
+        Vector2 size = font.MeasureString(text);
+        this.bounds = new Rectangle((int)position.X, (int)position.Y, (int)size.X, (int)size.Y);
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        MouseState mouseState = Mouse.GetState();
+        Point mousePosition = new Point(mouseState.X, mouseState.Y);
+
+        isHovered = bounds.Contains(mousePosition);
+        if (isHovered && mouseState.LeftButton == ButtonState.Pressed)
+        {
+            isClicked = true;
+        }
+        else if (isClicked && mouseState.LeftButton == ButtonState.Released)
+        {
+            isClicked = false;
+            action.Invoke();
+        }
+    }
+
+    public override void Draw(GameTime gameTime)
+    {
+        Color color = isHovered ? Color.RosyBrown : Color.White; // Change color on hover
+        Draw9Slice(texture, bounds, null, 10); // 10 is border size example
+        SpriteBatchManager.Instance.DrawString(font, text, position, Color.Black);
+    }
+}
---
+++ Classes/UI/UIElements/Toggle.cs
@@ -0,0 +1,64 @@
+﻿using HandsOnDeck;
+using HandsOnDeck.Classes.Managers;
+using HandsOnDeck.Classes.UI.UIElements;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+public class Toggle
+{
+    private Vector2 position;
+    private Action<bool> action;
+    private SpriteFont font;
+    private Texture2D texture;
+    private Texture2D hoverTexture;
+    private Rectangle bounds;
+    private bool isToggled;
+    private bool isHovered;
+    private bool isClicked;
+    private string label;
+
+    public Toggle(string label, Vector2 position, bool initialState, Action<bool> action)
+    {
+        this.label = label;
+        this.position = position;
+        this.action = action;
+        this.font = Game1.DefaultFont;
+        this.texture = ContentLoader.Load<Texture2D>("button");
+        this.hoverTexture = ContentLoader.Load<Texture2D>("buttonH");
+        this.isToggled = initialState;
+
+        Vector2 size = font.MeasureString(label);
+        this.bounds = new Rectangle((int)position.X, (int)position.Y, (int)size.X + 20, (int)size.Y);
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        MouseState mouseState = Mouse.GetState();
+        Point mousePosition = new Point(mouseState.X, mouseState.Y);
+
+        isHovered = bounds.Contains(mousePosition);
+        if (isHovered && mouseState.LeftButton == ButtonState.Pressed)
+        {
+            isClicked = true;
+        }
+        else if (isClicked && mouseState.LeftButton == ButtonState.Released)
+        {
+            isClicked = false;
+            isToggled = !isToggled;
+            action.Invoke(isToggled);
+        }
+    }
+
+    public override void Draw(GameTime gameTime)
+    {
+        Color toggleColor = isToggled ? Color.Green : Color.Red;
+        Color labelColor = isHovered ? Color.Gray : Color.White;
+        SpriteBatchManager.Instance.Draw(texture, bounds, labelColor);
+        SpriteBatchManager.Instance.DrawString(font, label, position, Color.Black);
+
+        Rectangle toggleButtonBounds = new Rectangle(bounds.Right + 5, bounds.Y, bounds.Height, bounds.Height);
+        SpriteBatchManager.Instance.Draw(texture, toggleButtonBounds, toggleColor);
+    }
+}
