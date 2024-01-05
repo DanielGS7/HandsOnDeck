98ab65059ab09966cde4e3ca8d44fe65cb007f40
afissou <afissou8@gmail.com>
2024-01-05 18:18:59 +0100

    implement cannonball behavior

--- Classes/Object/Entity/CannonBall.cs
+++ Classes/Object/Entity/CannonBall.cs
@@ -12,24 +12,30 @@ namespace HandsOnDeck.Classes.Object.Entity
     internal class CannonBall : CollidableGameObject, IEntity
     {
         private Animation cannonBall;
-        public CannonBall()
+        public Vector2 velocity { get; private set; }
+        public Vector2  position { get; set; }
+        public static float BaseSpeed = 5.0f;
+        public CannonBall(Vector2 initialPosition, Vector2 initialVelocity)
         {
-            
+            cannonBall = new Animation("cannonball",new Vector2(287,175),0,1,1,0,false);
+            position = initialPosition;
+            velocity = initialVelocity;
         }
 
         public override void LoadContent()
         {
-            
+            cannonBall.LoadContent();
         }
 
         public override void Update(GameTime gameTime)
         {
-            
+            position += velocity;
+            cannonBall.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime)
         {
-
+            cannonBall.Draw(position,0.2f,0,new Vector2(96,58));
         }
     }
 }
