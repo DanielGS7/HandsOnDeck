1339cc827dc1b402f06cbfb81002f96d62236218
afissou <afissou8@gmail.com>
2024-01-07 00:47:47 +0100

    Add shooting to enemy ship

--- Classes/Object/Entity/EnemyBoat.cs
+++ Classes/Object/Entity/EnemyBoat.cs
@@ -24,7 +24,10 @@ namespace HandsOnDeck.Classes.Object.Entity
         private readonly Random random;
         private const float EncircleDistanceMin = 100f;
         private const float EncircleDistanceMax = 200f;
-
+        bool canShoot = true;
+        private float shotCooldown = 2.0f;
+        private float currentCooldown = 0.0f;
+        CannonBalls cannonBalls = new CannonBalls();
         public EnemyBoat(Vector2 startPosition)
         {
             position = startPosition;
@@ -43,11 +46,24 @@ namespace HandsOnDeck.Classes.Object.Entity
         {
             UpdateMovement(gameTime, Player.GetInstance());
             boatSprite.Update(gameTime);
+            cannonBalls.Update(gameTime);
+
+            if (!canShoot)
+            {
+                currentCooldown -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+                if (currentCooldown <= 0)
+                {
+                    canShoot = true;
+                    currentCooldown = 0;
+                }
+            }
         }
 
         public override void Draw(GameTime gameTime)
         {
             boatSprite.Draw(position, 0.2f, rotation, new Vector2(597, 353));
+            cannonBalls.Draw(gameTime);
         }
 
         private void UpdateMovement(GameTime gameTime, Player player)
@@ -55,47 +71,72 @@ namespace HandsOnDeck.Classes.Object.Entity
             Vector2 directionToPlayer = player.position - position;
             float distanceToPlayer = directionToPlayer.Length();
 
-           
-            float noise = ((float)random.NextDouble() - 0.5f) * 0.02f; 
+            float noise = ((float)random.NextDouble() - 0.5f) * 0.02f;
 
-            
+
             float targetRotation = (float)Math.Atan2(directionToPlayer.Y, directionToPlayer.X);
 
-            
+
             if (distanceToPlayer > EncircleDistanceMax || distanceToPlayer < EncircleDistanceMin)
             {
-                
+
                 rotation = TurnTowards(rotation, targetRotation + noise, turnSpeedCoefficient);
 
-                
                 float targetSpeed = (distanceToPlayer < EncircleDistanceMin) ? -maxSpeed / 3 : maxSpeed;
                 speed = AdjustSpeed(speed, targetSpeed);
             }
             else
             {
-                
-                rotation = TurnTowards(rotation, targetRotation + MathHelper.PiOver2 + noise, turnSpeedCoefficient); 
-                speed = AdjustSpeed(speed, maxSpeed / 2); 
+                rotation = TurnTowards(rotation, targetRotation + MathHelper.PiOver2 + noise, turnSpeedCoefficient);
+                speed = AdjustSpeed(speed, maxSpeed / 2);
             }
 
-            
+            if (canShoot)
+            {
+
+
+                if (distanceToPlayer < EncircleDistanceMax && distanceToPlayer > EncircleDistanceMin)
+                {
+                    float relativeAngle = MathHelper.WrapAngle((float)Math.Atan2(directionToPlayer.Y, directionToPlayer.X) - rotation);
+
+                    if (relativeAngle > MathHelper.PiOver2 && relativeAngle < 3 * MathHelper.PiOver2)
+                    {
+                        ShootCannonBall(new Vector2((float)Math.Cos(rotation + MathHelper.PiOver2), (float)Math.Sin(rotation + MathHelper.PiOver2)));
+                    }
+                    else
+                    {
+                        ShootCannonBall(new Vector2((float)Math.Cos(rotation - MathHelper.PiOver2), (float)Math.Sin(rotation - MathHelper.PiOver2)));
+                    }
+                }
+
+            }
             Vector2 direction = new Vector2((float)Math.Cos(rotation), (float)Math.Sin(rotation));
             position += direction * speed;
         }
 
-        private float TurnTowards(float currentRotation, float targetRotation, float turnSpeed)
-        {
-            
-            float deltaRotation = MathHelper.WrapAngle(targetRotation - currentRotation);
-            return currentRotation + MathHelper.Clamp(deltaRotation, -turnSpeed, turnSpeed);
-        }
 
-        private float AdjustSpeed(float currentSpeed, float targetSpeed)
+        private void ShootCannonBall(Vector2 direction)
         {
-            return currentSpeed < targetSpeed
-                ? Math.Min(currentSpeed + accelerationRate, targetSpeed)
-                : Math.Max(currentSpeed - decelerationRate, targetSpeed);
+            cannonBalls.AddCannonball(position, direction * (speed + CannonBall.BaseSpeed));
+            canShoot = false;
+            currentCooldown = shotCooldown;
         }
+        private float TurnTowards(float currentRotation, float targetRotation, float turnSpeed)
+            {
+
+                float deltaRotation = MathHelper.WrapAngle(targetRotation - currentRotation);
+                return currentRotation + MathHelper.Clamp(deltaRotation, -turnSpeed, turnSpeed);
+            }
+
+            private float AdjustSpeed(float currentSpeed, float targetSpeed)
+            {
+                return currentSpeed < targetSpeed
+                    ? Math.Min(currentSpeed + accelerationRate, targetSpeed)
+                    : Math.Max(currentSpeed - decelerationRate, targetSpeed);
+            }
+
     }
 }
+
+
 
