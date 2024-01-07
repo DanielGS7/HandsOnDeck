2ad3e1f10cf08f5932ece4ec23fc0c2fd8f40659
afissou <afissou8@gmail.com>
2024-01-07 15:06:51 +0100

    add explosive barrel class

---
+++ Classes/Object/Entity/ExplosiveBarrel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnDeck.Classes.Object.Entity
+{
+    internal class ExplosiveBarrel : CollideableGameObject
+    {
+        public override void Draw(GameTime gameTime)
+        {
+            
+        }
+
+        public override void LoadContent()
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            
+        }
+    }
+}
