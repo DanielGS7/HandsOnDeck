3385fefe9781b397303934402da8f59acbd6ff46
afissou <afissou8@gmail.com>
2024-01-07 21:38:52 +0100

    add logic to explosive barrel

--- Classes/Object/Entity/ExplosiveBarrel.cs
+++ Classes/Object/Entity/ExplosiveBarrel.cs
@@ -5,30 +5,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace HandsOnDeck.Classes.Object.Entity
 {
     internal class ExplosiveBarrel : CollideableGameObject
     {
         private Animation explosiveBarrel = new Animation("Unstable_Barrel",new Vector2(360,360),0,1,1,0, false);
-        public ExplosiveBarrel() 
-        {
+        private float activationRange = 100f;
 
+        public ExplosiveBarrel(Vector2 startPosition) 
+        {
+            position = startPosition;
         }
 
-        public override void Draw(GameTime gameTime)
-        {
-            
-        }
+       
 
         public override void LoadContent()
         {
-
+            explosiveBarrel.LoadContent();
         }
 
         public override void Update(GameTime gameTime)
         {
-            
+            explosiveBarrel.Update(gameTime);
+        }
+
+
+        public override void Draw(GameTime gameTime, Player player)
+        {
+            if(player.position - position< activationRange)
+            {
+                explosiveBarrel.Draw(position);
+            }
+
         }
     }
 }
