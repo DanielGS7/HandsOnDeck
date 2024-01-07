aca91cd6119d0b2157cb45cfd4d5006f3a135c0c
Gh0stage <61917966+Gh0stage@users.noreply.github.com>
2024-01-07 21:55:20 +0100

    Add CollisionManager Singleton and modify CollideableGameObject

---
+++ Classes/Managers/CollisionManager.cs
@@ -0,0 +1,57 @@
+﻿using HandsOnDeck.Classes.Object;
+using System.Collections.Generic;
+
+namespace HandsOnDeck.Classes.Managers
+{
+    public class CollisionManager
+    {
+        private static CollisionManager instance;
+        private static readonly object lockObject = new object();
+
+        internal List<CollideableGameObject> gameObjects;
+        public static CollisionManager GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if (instance == null)
+                            instance = new CollisionManager();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private CollisionManager()
+        {
+            gameObjects = new List<CollideableGameObject>();
+        }
+
+        internal void AddCollideableObject(CollideableGameObject gameObject)
+        {
+            gameObjects.Add(gameObject);
+        }
+
+        internal void CheckForCollisions()
+        {
+            for (int i = 0; i < gameObjects.Count; i++)
+            {
+                for (int j = i + 1; j < gameObjects.Count; j++)
+                {
+                    var gameObjectA = gameObjects[i];
+                    var gameObjectB = gameObjects[j];
+
+                    if (gameObjectA.Hitbox.bounds.Intersects(gameObjectB.Hitbox.bounds))
+                    {
+                        gameObjectA.onCollision(gameObjectB);
+                        gameObjectB.onCollision(gameObjectA);
+                    }
+                }
+            }
+        }
+    }
+
+}
